f70100f4e1229bec29b64392b2ffb9192323bee4
Jonathan Lauxen Romano <jonathan@Emily-3.0>
2022-04-18 23:19:37 -0300

    vai rodar?

--- src/controllers/endpoints.py
+++ src/controllers/endpoints.py
@@ -7,6 +7,16 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 from src.server.instance import server
 
+cookie_list = [
+    {'name': '_gat', 'value': '1', 'path': '/', 'domain': '.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1650318979, 'sameSite': 'None'},
+    {'name': 'voxtecnologia-consent-cookie', 'value': 'MQ==', 'path': '/', 'domain': 'www.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1681854920, 'sameSite': 'None'},
+    {'name': 'voxtecnologia-initial-message', 'value': 'IjA2LzA0LzIwMjIi', 'path': '/', 'domain': 'www.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1651614921, 'sameSite': 'None'},
+    {'name': 'sigfacil', 'value': 'f6401f840ad4b780ef81e63361c461aa', 'path': '/', 'domain': 'www.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': True, 'sameSite': 'None'},
+    {'name': 'hgyclh-w7930', 'value': '9fa8eb2cfa2839b6ff99e6340d1c9de4', 'path': '/', 'domain': '.www.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1681854933, 'sameSite': 'None'},
+    {'name': '_ga', 'value': 'GA1.4.1176697986.1650318920', 'path': '/', 'domain': '.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1713390933, 'sameSite': 'None'},
+    {'name': '_gid', 'value': 'GA1.4.469418369.1650318920', 'path': '/', 'domain': '.empresafacil.pr.gov.br', 'secure': False, 'httpOnly': False, 'expiry': 1650405333, 'sameSite': 'None'}
+]
+
 app, api = server.app, server.api
 
 @api.route('/empresaFacil')
@@ -22,8 +32,6 @@ class empresaFacil(Resource):
         x = body['x']
 
         driver.get('https://www.jonathanromano.online/')
-
-        input('teste >>')
 
         y = driver.find_element_by_xpath('/html/body/div/div/main/div[1]/h1').text
         
